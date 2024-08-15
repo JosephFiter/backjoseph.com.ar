dcc657afe25d8509921bdc029ce83c1d91c0bff6
Lucasszejn <roguenine6@gmail.com>
2024-08-15 20:18:47 -0300

    index

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 const app = express();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = "https://eflbswavtctlxszlnzks.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmbGJzd2F2dGN0bHhzemxuemtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2ODQ1MTcsImV4cCI6MjAzOTI2MDUxN30.vqBOuy7zozGPbilbNexOdACWnoVnx8R6irlLuBo7vK4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Middleware para parsear JSON
